d7cfcb3f9e3139d63235ece69292d87a61649bbe
Joselino Santos <joselinosantosti@gmail.com>
2022-12-09 12:11:21 -0300

    Criado componente criar-ideia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componentes/header/header.component';
 import { FooterComponent } from './componentes/footer/footer.component';
+import { CriarIdeiaComponent } from './componentes/ideias/criar-ideia/criar-ideia.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    CriarIdeiaComponent
   ],
   imports: [
     BrowserModule,
