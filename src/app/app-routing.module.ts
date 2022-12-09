aa4a1a5f2041e2472d5cf8e46bf11426d00f4a36
Joselino Santos <joselinosantosti@gmail.com>
2022-12-09 14:04:28 -0300

    Criadas rotas para páginas listar, criar e home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
+import { CriarIdeiaComponent } from './componentes/ideias/criar-ideia/criar-ideia.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListarIdeiasComponent } from './componentes/ideias/listar-ideias/listar-ideias.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'listar-ideias',
+    pathMatch: 'full'
+  },
+  {
+    path: 'criar-ideia',
+    component: CriarIdeiaComponent
+  },
+  {
+    path: 'listar-ideias',
+    component: ListarIdeiasComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
