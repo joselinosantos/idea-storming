dba4a83810fb6a3ea8be9eade1b3cf9e7a3b52ae
Joselino Santos <joselinosantosti@gmail.com>
2022-12-12 10:58:11 -0300

    Desabilitar botão quando formulário for inválido

--- src/app/componentes/ideias/criar-ideia/criar-ideia.component.ts
+++ src/app/componentes/ideias/criar-ideia/criar-ideia.component.ts
@@ -27,7 +27,7 @@ export class CriarIdeiaComponent implements OnInit {
     })
   }
 
-  criarIdeia() {    
+  criarIdeia() {
     if(this.formulario.valid) {
       this.service.criar(this.formulario.value).subscribe(() => {
         this.router.navigate(['/listar-ideias'])
@@ -37,6 +37,13 @@ export class CriarIdeiaComponent implements OnInit {
 
   cancelar() {
     this.router.navigate(['/listar-ideias'])
+  }
+
+  habilitarBtn(): string {
+    if (this.formulario.valid) {
+      return 'btn-salva'
+    }
+    return ''
   }
 
 }
