e8bf319c4d6230df3ff784485e614cdc662bcd51
Joselino Santos <joselinosantosti@gmail.com>
2022-12-12 10:34:15 -0300

    Implementada validação para espaços vazios e tamanho mínimo

--- src/app/componentes/ideias/criar-ideia/criar-ideia.component.ts
+++ src/app/componentes/ideias/criar-ideia/criar-ideia.component.ts
@@ -21,14 +21,14 @@ export class CriarIdeiaComponent implements OnInit {
 
   ngOnInit(): void {
     this.formulario = this.formBuilder.group({
-      conteudo: ['', [Validators.required]],
-      autor: ['', [Validators.required]],
+      conteudo: ['', Validators.compose([Validators.required, Validators.pattern(/(.|\s)*\S(.|\s)*/)])],
+      autor: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
       modelo: ['modelo1']
     })
   }
 
-  criarIdeia() {
-if(this.formulario.valid) {
+  criarIdeia() {    
+    if(this.formulario.valid) {
       this.service.criar(this.formulario.value).subscribe(() => {
         this.router.navigate(['/listar-ideias'])
       })
